a0645065e03def7d0f5be80b143d18ed03ab0bb6
V.Santhosh <37298157+santhoshtheges@users.noreply.github.com>
2020-12-18 16:23:01 +0530

    Update MyPlexer.py

--- MyPlexer.py
+++ MyPlexer.py
@@ -3,10 +3,10 @@ app=MyPlexer()
 @app.route('/')
 def hello_world():
     input("Input the command to be executed")
-    return 'Hello, World!'
+    print('Hello, World!')
 
 @app.route('/')
 def bye():
-    return 'bye'
+    print('bye')
 
 app.run()
